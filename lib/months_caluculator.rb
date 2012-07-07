06c48e139200b721c997bc8b867479682ad1ee4c
Sateesh Kambhampati <sateesh.k09@gmail.com>
2012-07-07 18:56:08 +0530

    Adding Business logic for caluculating months

--- lib/months_caluculator.rb
+++ lib/months_caluculator.rb
@@ -2,4 +2,27 @@ require "months_caluculator/version"
 
 module MonthsCaluculator
   # Your code goes here...
+  def self.description
+    p "To Caluculate Exact Number of Months We Will Use this gem"
+  end
+  
+  def self.get_exact_number_of_months(from_date, to_date)
+    number_of_months = 0
+    if from_date.blank? && to_date.blank?
+      raise "Please Enter From and To Dates"
+    elsif from_date.blank?
+      raise "Please Enter From Date"
+    elsif to_date.blank?
+      raise "Please Enter To Date"
+    elsif from_date.present?  && to_date.present?
+      from_date = from_date.to_date
+      to_date =   to_date.to_date
+      number_of_months = (to_date.year*12+to_date.month) - (from_date.year*12+from_date.month)
+      end_date = from_date.to_date + number_of_months.months
+      number_of_days =  to_date.day-end_date.day
+      # If the number of days in month is more than 15 then we will assume it as one month and we add it below.
+      number_of_months = number_of_months + 1  if number_of_days >=15
+    end
+    number_of_months 
+  end
 end
--- months_caluculator.gemspec
+++ months_caluculator.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |s|
   s.authors     = ["Sateesh Kambhampati"]
   s.email       = ["sateesh.k09@gmail.com"]
   s.homepage    = ""
-  s.summary     = %q{TODO: Write a gem summary}
-  s.description = %q{TODO: Write a gem description}
+  s.summary     = "Purpose is to caluculate exact number of months between two dates"
+  s.description = "A simple gem to caluculate exact number of months"
 
   s.rubyforge_project = "months_caluculator"
 
